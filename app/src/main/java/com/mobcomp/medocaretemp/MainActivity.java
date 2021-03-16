b747148c67928e0b167d24ed2c879d32cf514fb5
Drishti De <drishti20075@iiitd.ac.in>
2021-03-16 15:07:38 +0530

    Recents

--- app/src/main/java/com/mobcomp/medocaretemp/MainActivity.java
+++ app/src/main/java/com/mobcomp/medocaretemp/MainActivity.java
@@ -25,23 +25,20 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    //private FloatingActionButton mAddAlarmFab, mUploadPrescription;
-    private FloatingActionButton fab;
-    //TextView addAlarmActionText, uploadPrescriptionText;
-    //Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_changepwd, R.id.nav_help, R.id.nav_logout)
+                R.id.nav_home,R.id.nav_profile, R.id.nav_changepwd, R.id.nav_help, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -53,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
+
                     case R.id.set_alarm_btn:
                         Toast.makeText(MainActivity.this, "Set Alarm clicked", Toast.LENGTH_SHORT).show();
                         break;
